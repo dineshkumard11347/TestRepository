96bb9513e6cbef08726ff6cdb2090b43f0e50175
dineshkumard11347 <58125427+dineshkumard11347@users.noreply.github.com>
2019-12-25 20:32:33 +0530

    First commit

---
+++ TestConnection/src/Register.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.sql.*;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+public class Register extends HttpServlet {
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Inside post method..");
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		System.out.println("Request reached servlet..");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		String userName = request.getParameter("userName");
+		String userPass = request.getParameter("userPass");
+		String userEmail = request.getParameter("userEmail");
+		String userCountry = request.getParameter("userCountry");
+		System.out.println("userName::"+userName);
+		System.out.println("userPass::"+userPass);
+		System.out.println("userEmail::"+userEmail);
+		System.out.println("userCountry::"+userCountry);
+		
+		RequestDispatcher disp = null;
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			System.out.println("Connecting to a selected database...");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/TestDB", "root", "P@ssword123");
+			System.out.println("Connected database successfully...");
+			
+			Statement stmt = con.createStatement();
+			 
+			  String sql = "INSERT INTO Registration VALUES (null, '"+userName+"','"+userPass+"','"+userEmail+"','"+userCountry+"')";
+			  System.out.println("sql::"+sql);
+			  stmt.executeUpdate(sql);
+		      
+		      System.out.println("Inserted records into the table...");
+			
+		      request.setAttribute("response", "Record added successfully");
+		      
+		      out.print("Record added successfully");  
+		      disp = request.getRequestDispatcher("/success.html");
+		      disp.forward(request, response);
+
+		} catch (Exception e2) {
+			e2.printStackTrace();
+			disp = request.getRequestDispatcher("/failure.html");
+		    disp.forward(request, response);
+		}
+
+		out.close();
+	}
+
+}
